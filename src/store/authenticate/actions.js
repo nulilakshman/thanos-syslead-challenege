118c54f9888981f6eedb8fb20213e1add78a3a9d
lakshmannuli <lakshmanjoyforever@gmail.com>
2021-02-13 14:27:29 +0530

    authentication

--- src/store/authenticate/actions.js
+++ src/store/authenticate/actions.js
@@ -6,11 +6,30 @@ import {
 export const actions = {
     validateUser: (user, history) => dispatch => {
         if (user.userName === 'admin@thanos.com' && user.password === 'admin@123') {
-            const details = { ...user, isAuthenticated: true, role: 1 }
+            const details = { ...user, isAuthenticated: true, roleId: 'admin' }
             localStorage.setItem("AUTH_USER", JSON.stringify(details))
             dispatch({ type: LOGIN_SUCCESS, details });
             history.push('/manage-organizations');
             return;
+        }
+        else {
+            let allUsers = localStorage.getItem("USERS");
+            if (allUsers === null) {
+                allUsers = [];
+            }
+            else {
+                allUsers = JSON.parse(allUsers)
+            }
+            const loggedInUser = allUsers.filter(x => x.userName === user.userName && user.password === x.password);
+
+            if (loggedInUser !== null && loggedInUser.length > 0) {
+                const details = { ...loggedInUser[0], isAuthenticated: true }
+                localStorage.setItem("AUTH_USER", JSON.stringify(details))
+                dispatch({ type: LOGIN_SUCCESS, details: details });
+                history.push(`/manage-users/${details.organizationId}`);
+                return;
+            }
+
         }
         const details = { ...user, isAuthenticated: false }
         dispatch({ type: LOGIN_ERROR, details });
